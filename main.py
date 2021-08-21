221b1b2c7523b4a4f5bc3a676f3b4dfc42dd4182
Nchia-Emmanuela <83321016+Nchia-Emmanuela@users.noreply.github.com>
2021-08-21 13:03:29 +0100

    encryption function

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o',
 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 'y', 'z']
+key = int(input("Enter your key: \n"))
+choice = input("Type 'Encode' to Encrype or 'Decode' to Decrypt: \n")
+text = input("Enter message: \n")
+
+def Encrpyt(key, text):
+    
